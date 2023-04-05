0d4b95049eacb2e4551067a5749723987334a57b
getacupoftea <tezebada@gmail.com>
2023-04-05 09:50:43 +0100

    po

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 
+
+int  nroot(int n, int count);
+
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
  * @n: The number to return the square root of.
@@ -9,43 +12,45 @@
  *         If n does not have a natural square root - -1.
  */
 
-int root = 0;
-int count = 1;
-
-void setglobal(void)                    {                                       root = 0;;                              }
 
 int _sqrt_recursion(int n)
 {
-int count = n;
-int ans;
-int store;
 
-if (root == 0)
-{
- root  = n;
- count = 1;
+return (nroot(n, 1));
 }
+
+/**                                   * root - Returns the natural square root of a number.
+ * @n: The number to return the squar
+e root of.
+ *  @count: increment
+ *  Return: If n has a natural squar
+ root - the natural square root of n
+ If n does not have a natural square root - -1.
+ */
+
+int nroot(int n, int count) 
+{
+int store = n;
+int ans;
+int root = n;
+
 if (root <=  0)
 {
-setglobal();
 return (-1);
 }
 ans = count * count;
 if (ans == root)
 {
 store = count;
-setglobal();
         return (count);
 }
 else if (ans  > root)
 {                        
-	setglobal();
 	return (-1);       
 }
 else
 {
-return( _sqrt_recursion(count + 1));
+return(nroot(store, count + 1));
 }
-setglobal();
 return (count);
 }
