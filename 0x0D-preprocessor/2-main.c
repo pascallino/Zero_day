04094a233cc7576239a853819fa74fd8576dc492
getacupoftea <tezebada@gmail.com>
2023-04-17 07:57:28 +0100

    ok

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints the file name
+ * Return: 0
+ */
+
+int main() {
+    printf("%s\n", __FILE__);
+    return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * Header File SUM_H
+ * Description: Header file that defines a funvtion  macro named 
+ * SUM is an abbreviation for the toke
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
